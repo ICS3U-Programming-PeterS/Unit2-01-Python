8367605658a4821c71c1b33f2763cddbbb2fe6d6
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2022-09-22 02:40:44 +0000

    Added area_per_circle.py

--- area_per_circle.py
+++ area_per_circle.py
@@ -12,8 +12,8 @@ import math
 
 def main():
     print("For a circle with the radius 15mm: \n")
-    print("The area is: {} mm^2". format(math.pi*15**2))
-    print("The circumference is: {} mm". format(2*math.pi*15))
+    print("The area is: {} mm^2".format(math.pi * 15**2))
+    print("The circumference is: {} mm".format(2 * math.pi * 15))
 
 
 if __name__ == "__main__":
